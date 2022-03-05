eda6886a9086cc463385df9d3eee7726c4ad7e24
caliLai <calilai2018@gmail.com>
2022-03-05 03:09:35 -0800

    minor additions for controller and routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,9 +7,11 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 class RouteServiceProvider extends ServiceProvider
 {
+	protected $namespace = 'App\Http\Controllers';
     /**
      * The path to the "home" route for your application.
      *
