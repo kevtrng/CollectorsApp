507a0de431ba48e21c9bac14ced12912c6e08edb
Kev Truong <kevtrng00@gmail.com>
2022-03-22 02:12:26 -0700

    register page:

--- resources/views/home/register.blade.php
+++ resources/views/home/register.blade.php
@@ -2,14 +2,22 @@
 @extends('layouts.app')
 @section('content')
 <html>
+<link href="{{ asset('css/login.css') }}" rel="stylesheet">		
 	<body>
-		Register
-		<form method="post" action="/home/submitRegister" name="registerForm">
-			<input type="text" name="username" placeholder ="username"/><br>
-			<input type="email" name="email" placeholder ="email"/><br>
-			<input type="password" name="password" placeholder ="password"/><br>
-			<input type="submit" />
-		</form>
+		<div class="container">
+			<div class="formstuff">
+				<h1>Register</h1>
+				<h4>Create your account!</h4>
+				<form method="post" action="/home/submitRegister" name="registerForm">
+					<input type="text" name="username" placeholder ="username"/><br>
+					<input type="email" name="email" placeholder ="email"/><br>
+					<input type="password" name="password" placeholder ="password"/><br>
+					<input type="submit" />
+				</form>
+			</div>
+			<div class=imagestuff></div>
+
+		</div>
 	</body>
 </html>
 @endsection
