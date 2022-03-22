2dea560eef739cff33787e9664cc9e9a4dfa3716
Kev Truong <kevtrng00@gmail.com>
2022-03-22 01:31:18 -0700

    log in page css

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -1,14 +1,28 @@
+
 <!DOCTYPE html>
 @extends('layouts.app')
 @section('content')
 <html>
+	<link href="{{ asset('css/login.css') }}" rel="stylesheet">
 	<body>
-		Login
-		<form method="post" action="/home/submitLogin" name="loginForm">
-			<input type="email" name="email" placeholder="email"/><br>
-			<input type="password" name="password" placeholder="password"/><br>
-			<input type="submit" />
-		</form>
+		<div class="container">
+			<div class="formstuff">
+				<h1>Welcome to Collector!</h1>
+				<h4>Log in using your email and password</h4>
+				<form method="post" action="/home/submitLogin" name="loginForm">
+					<input type="email" name="email" placeholder="email"/><br>
+					<input type="password" name="password" placeholder="password"/><br>
+					<input type="submit" />
+				</form>
+			</div>
+
+			<div class="imagestuff"></div>
+
+		</div>
+
+		
+
+
 	</body>
 </html>
 @endsection
