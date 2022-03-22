a608d9514f27bf1922d561f1be894e8c65d5be87
Octavio Villena <octaviovillena@hotmail.com>
2022-03-22 02:17:14 -0700

    added some controllers

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	for things that aren't for authenticated users
+	- login
+	- registration
+	- home page (before login)
+*/
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function login() {
+		return view('/home/login');
+	}
+
+    public function register() {
+		return view('/home/register');
+	}
+
+	public function landing() {
+		return view ('home/landing');
+	}
+
+
+}
