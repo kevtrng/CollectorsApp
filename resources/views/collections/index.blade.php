2d7c4e6fce3f25aa37b40ca568ad2987eaed5bc5
Octavio Villena <octaviovillena@hotmail.com>
2022-03-27 22:08:34 -0700

    collections view html

--- resources/views/collections/index.blade.php
+++ resources/views/collections/index.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 @section('content')
 
-    app to keep track of your inventory as a collector
+<button>Create collection</button>
+    <div>
+{{--        foreach this div--}}
+        <h2>title of collection</h2>
+        <ul>
+            <li>collection item</li>
+            <li>collection item</li>
+            <li>collection item</li>
+            <li>collection item</li>
+        </ul>
+        <button>view full list</button>
+        <p>total worth: $1000</p>
+    </div>
 
 @endsection
