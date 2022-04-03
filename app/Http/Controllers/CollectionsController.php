c4852b878e6dfd09eeda2f9840002e47b623fe7e
Hongsoon Kim <khsun6226@naver.com>
2022-04-03 05:41:03 -0700

    show collections and lists dynamically

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -8,16 +8,19 @@ use App\Models\Collection;
 class CollectionsController extends Controller
 {
 
-    public function index() {
-        $collections = Collection::where('userEmail', 'test@test.com')->get();
+    public function index(Request $request) {
+      $currentUser = $request->user()->email;
+        $collections = Collection::where('userEmail', $currentUser)->get();
 //        $collections = Collection::all();
 //        $collections = $collections->toArray();
         $type = gettype($collections);
 		return view('/collections/index', ['collections'=> $collections, 'type'=>$type]);
 	}
 
-    public function list($id) {
-        return view('/collections/list');
+    public function list(Request $request) {
+      $currentCollection = $request->name;
+      $collection = Collection::where('collectionName', $currentCollection)->get();
+      return view('/collections/list', ['collectionName' => $currentCollection, 'collection' => $collection]);
     }
     public function createCollection() {
         return view('/collections/list/createcollection');
@@ -35,11 +38,16 @@ class CollectionsController extends Controller
 
     }
 
-    public function addItem() {
-        return view('/collections/list/additem');
+    public function addItem(Request $request) {
+        return view('/collections/additem', ['collection' => $request->name]);
     }
 
     public function addItemPost(Request $request) {
+      $collectionName = $request->name;
+      $collection = Collection::where('collectionName', $collectionName)->get();
+      Collection::where('collectionName', $collectionName)->push('items', array('itemName' => $request->itemName));
 
+      // return redirect("/collections/list", 302, ['collectionName' => $request->name, 'collection' => $collection]);
+      return redirect("/collections");
     }
 }
